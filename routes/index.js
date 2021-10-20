65a502a3db4c2607df46ed1b9e4c9cef931b3b1b
Felpe <felipe.e.salom@gmail.com>
2021-10-20 20:04:19 -0300

    califications Controller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const publicationRoutes = require("./publicationRoutes");
 const peopleRoutes = require("./peopleRoutes");
+const calificationRoutes = require('./calificationsRoutes')
 
 const router = express.Router();
 router.use("/publications", publicationRoutes);
 router.use("/people", peopleRoutes);
+router.use('/califications', calificationRoutes);
 
 module.exports = router;
