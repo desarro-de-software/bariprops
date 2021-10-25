af3196e134977e722d03e65a830c46a24ca8cdb8
Maia <maiajaelsosa@gmail.com>
2021-10-25 19:16:15 -0300

    Carga de userController, userRoutes y users.yaml

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const publicationRoutes = require("./publicationRoutes");
 const peopleRoutes = require("./peopleRoutes");
+const userRoutes = require('./userRoutes');
 
 const router = express.Router();
 router.use("/publications", publicationRoutes);
 router.use("/people", peopleRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
