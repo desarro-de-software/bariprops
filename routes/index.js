175aadf1b7fe02adf1fbde7ffb32aabeecc27613
agustinsara <81448175+agustinsara@users.noreply.github.com>
2021-11-01 19:58:41 -0300

    Califications  (#10)
    * califications Controller
    
    * removiendo los conflictos que tuve
    
    Co-authored-by: Felpe <felipe.e.salom@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const publicationRoutes = require("./publicationRoutes");
 const peopleRoutes = require("./peopleRoutes");
 const userRoutes = require('./userRoutes');
+const calificationRoutes = require('./calificationsRoutes')
 
 const router = express.Router();
 router.use("/publications", publicationRoutes);
 router.use("/people", peopleRoutes);
 router.use('/users', userRoutes);
+router.use('/califications', calificationRoutes);
 
 module.exports = router;
